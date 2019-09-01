1205f0bb5d4425f6946827c2b27a3b652e050fe4
philblanchard <phil@philblanchard.com>
2019-09-01 11:59:30 -0400

    done basic setup

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES
+    ('Phil Burger', false),
+    ('SQL Burger', false),
+    ('Spaghetti Burger', true),
+    ('Cheese Burger', false),
+    ('Watermelon Burger', false),
+    ('Bad Burger', true),
+    ('Good Burger', false);
